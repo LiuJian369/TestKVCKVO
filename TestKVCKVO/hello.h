ffd19d0afdc45ad94f9c8945a26c08f4c49aa9de
LiuJian369 <liujian@liujian111.local>
2016-08-09 19:21:03 +0800

    second

---
+++ TestKVCKVO/hello.h
@@ -0,0 +1,13 @@
+//
+//  hello.h
+//  TestKVCKVO
+//
+//  Created by 刘建 on 16/8/9.
+//  Copyright © 2016年 liujian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hello : NSObject
+
+@end
---
+++ TestKVCKVO/hello.m
@@ -0,0 +1,13 @@
+//
+//  hello.m
+//  TestKVCKVO
+//
+//  Created by 刘建 on 16/8/9.
+//  Copyright © 2016年 liujian. All rights reserved.
+//
+
+#import "hello.h"
+
+@implementation hello
+
+@end
